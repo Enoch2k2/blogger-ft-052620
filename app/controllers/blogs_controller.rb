07684a2b09bd2e5b1cd7c435f6948f422a27c4e2
Enoch2k2 <enoch2k2@hotmail.com>
2020-07-30 11:41:24 -0500

    removed code to resemble lecture 1

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,4 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, except: [:index, :new, :create]
 
   def index
     if params[:term]
@@ -8,46 +7,4 @@ class BlogsController < ApplicationController
       @blogs = Blog.sorted_published_blogs
     end
   end
-
-  def show
-  end
-
-  def new
-    @blog = Blog.new
-  end
-
-  def create
-    @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @blog.update(blog_params)
-      redirect_to blog_path(@blog)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @blog.destroy
-    redirect_to blogs_path
-  end
-  
-  private
-    def set_blog
-      @blog = Blog.find_by_id(params[:id])
-    end
-
-    def blog_params
-      params.require(:blog).permit(:title, :content, :published_date)
-    end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,8 @@
 Rails.application.routes.draw do
-  root to: "static#home"
-  resources :blogs
+  # resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # # index
-  # get "/blogs", to: "blogs#index", as: "blogs"
+  # index
+  get "/blogs", to: "blogs#index"
 
-  # # new / create
-  # get "/blogs/new", to: "blogs#new", as: "new_blog"
-  # post "/blogs", to: "blogs#create"
-
-  # # show
-  # get "/blogs/:id", to: "blogs#show", as: "blog"
-
-  # # edit / update
-
-  # get "/blogs/:id/edit", to: "blogs#edit", as: "edit_blog"
-  # patch "/blogs/:id", to: "blogs#update"
-
-  # # destroy
-
-  # delete "/blogs/:id", to: "blogs#destroy"
 end
